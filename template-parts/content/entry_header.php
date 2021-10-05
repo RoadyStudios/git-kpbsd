400de0d3d943415ed9f72e46730b537fa6850b00
RoadyStudios <aroady@me.com>
2021-10-05 09:11:14 -0800

    Post Category page changes

--- template-parts/content/entry_header.php
+++ template-parts/content/entry_header.php
@@ -9,17 +9,19 @@ namespace WP_Rig\WP_Rig;
 
 ?>
 
-	<?php
-	if ( ! is_front_page() && ! is_page() ) {
-		get_template_part( 'template-parts/content/entry_meta', get_post_type() );
-	}
+<?php
 
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
-	}
+if ( ! is_search() ) {
+	get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
+}
 
-	if ( ! is_front_page() ) {
-		echo ( '<header class="entry-header">' );
-		get_template_part( 'template-parts/content/entry_title', get_post_type() );
-	}
-		echo ( '</header><!-- .entry-header -->' );
+if ( ! is_front_page() ) {
+	echo ( '<header class="entry-header">' );
+	get_template_part( 'template-parts/content/entry_title', get_post_type() );
+}
+
+if ( ! is_front_page() && ! is_page() ) {
+	get_template_part( 'template-parts/content/entry_meta', get_post_type() );
+}
+
+echo ( '</header><!-- .entry-header -->' );
