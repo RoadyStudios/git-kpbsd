1d245b2031ea482f65472a2e8ed85fb5f6363c55
RoadyStudios <aroady@me.com>
2021-10-06 15:39:03 -0800

    tweaking

--- template-parts/content/entry_header.php
+++ template-parts/content/entry_header.php
@@ -11,10 +11,6 @@ namespace WP_Rig\WP_Rig;
 
 <?php
 
-if ( ! is_search() ) {
-	get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
-}
-
 if ( ! is_front_page() ) {
 	echo ( '<header class="entry-header">' );
 	get_template_part( 'template-parts/content/entry_title', get_post_type() );
@@ -22,6 +18,10 @@ if ( ! is_front_page() ) {
 
 if ( ! is_front_page() && ! is_page() ) {
 	get_template_part( 'template-parts/content/entry_meta', get_post_type() );
+}
+
+if ( ! is_search() ) {
+	get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
 }
 
 echo ( '</header><!-- .entry-header -->' );
