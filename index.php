c9666c1631d3c30239087c367280f88dbd3b2967
RoadyStudios <aroady@me.com>
2021-11-16 15:59:00 -0900

    Update index.php

--- index.php
+++ index.php
@@ -23,12 +23,12 @@ kpbsd_codebase()->print_styles( 'kpbsd-codebase-content' );
 		<?php
 		if ( have_posts() ) {
 			get_template_part( 'template-parts/content/page_header' );
-			echo "<div class='category-grid'>";
+
 			while ( have_posts() ) {
 				the_post();
-				get_template_part( 'template-parts/content/entry_category', get_post_type() );
+				get_template_part( 'template-parts/content/entry', get_post_type() );
 			}
-			echo '</div>';
+
 			if ( ! is_singular() ) {
 				get_template_part( 'template-parts/content/pagination' );
 			}
