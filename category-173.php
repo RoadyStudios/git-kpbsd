af3e1f67ade884119a75271f99c4081458c35f77
RoadyStudios <aroady@me.com>
2021-11-18 11:57:45 -0900

    Create category-173.php

---
+++ category-173.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The template for displaying category archives.
+ *
+ * When active, applies to all category archives.
+ * To target a specific category, rename file to category-{slug/id}.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#category
+ *
+ * @package kpbsd_codebase
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+kpbsd_codebase()->print_styles( 'kpbsd-codebase-content' );
+
+?>
+	<main id="primary" class="site-main">
+		<?php
+
+		$args = array(
+			'order'    => 'ASC',
+			'meta_key' => '_expiration_date',
+			'orderby'  => 'meta_value',
+		);
+
+		$scholar_query = new WP_Query( $args );
+
+		if ( $scholar_query->have_posts() ) {
+
+			get_template_part( 'template-parts/content/page_header' );
+
+			echo "<div class='category-grid'>";
+
+			while ( $scholar_query->have_posts() ) {
+				$scholar_query->the_post();
+				get_template_part( 'template-parts/content/entry_category', get_post_type() );
+			}
+
+			echo '</div>';
+
+			get_template_part( 'template-parts/content/pagination' );
+
+		} else {
+
+			get_template_part( 'template-parts/content/error' );
+
+		}
+		?>
+	</main><!-- #primary -->
+<?php
+get_sidebar();
+get_footer();
