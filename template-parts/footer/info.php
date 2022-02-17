913465dac1c55f3f2fb48427e081a452b972edc7
RoadyStudios <aroady@me.com>
2022-02-17 11:19:28 -0900

    branding and footer icon media folder location change

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -10,13 +10,13 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-footer-social">
-	<a href="http://mail.g.kpbsd.org/" title="link to gmail"><img src="https://kpbsd.org/wp-content/uploads/footer_gmail.png" alt="gmail logo" /></a>
-	<a href="http://drive.google.com/a/g.kpbsd.org/" title="link to google drive"><img src="https://kpbsd.org/wp-content/uploads/footer_google-drive.png" alt="google drive logo" /></a>
-	<a href="https://www.instagram.com/kenaipeninsulaschools/"><img src="https://kpbsd.org/wp-content/uploads/footer_instagram.png" alt="instagram logo"></a>
-	<a href="http://www.twitter.com/kpbsd" title="link to twitter"><img src="https://kpbsd.org/wp-content/uploads/footer_twitter.png" alt="twitter logo" /></a>
-	<a href="http://www.facebook.com/kpbsd" title="link to facebook"><img src="https://kpbsd.org/wp-content/uploads/footer_facebook.png" alt="facebook logo" /></a>
-	<a href="https://www.youtube.com/KPBSDvideos" title="link to youtube"><img src="https://kpbsd.org/wp-content/uploads/footer_youtube.png" alt="youtube logo" /></a>
-	<a href="https://kpbsd.zoom.us/" title="link to zoom"><img src="https://kpbsd.org/wp-content/uploads/footer_zoom.png" alt="zoom logo" /></a>
+	<a href="http://mail.g.kpbsd.org/" title="link to gmail"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_gmail.png" alt="gmail logo" /></a>
+	<a href="http://drive.google.com/a/g.kpbsd.org/" title="link to google drive"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_google-drive.png" alt="google drive logo" /></a>
+	<a href="https://www.instagram.com/kenaipeninsulaschools/"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_instagram.png" alt="instagram logo"></a>
+	<a href="http://www.twitter.com/kpbsd" title="link to twitter"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_twitter.png" alt="twitter logo" /></a>
+	<a href="http://www.facebook.com/kpbsd" title="link to facebook"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_facebook.png" alt="facebook logo" /></a>
+	<a href="https://www.youtube.com/KPBSDvideos" title="link to youtube"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_youtube.png" alt="youtube logo" /></a>
+	<a href="https://kpbsd.zoom.us/" title="link to zoom"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/footer_zoom.png" alt="zoom logo" /></a>
 </div><!-- .site-footer-social -->
 
 <div class="site-info">
--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -20,13 +20,9 @@ namespace WP_Rig\WP_Rig;
 
 	<div class="box-icon-wrapper">
 		<div class="quick-icons">
-			<a href="http://mail.g.kpbsd.org/"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-gmail.png" alt="gmail icon"></a>
-			<a href="http://drive.google.com/a/g.kpbsd.org/"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-google-drive.png" alt="google drive icon"></a>
-			<a href="https://www.instagram.com/kenaipeninsulaschools/"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-instagram.png" alt="instagram icon"></a>
-			<a href="http://www.facebook.com/kpbsd"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-facebook.png" alt="facebook icon"></a>
-			<a href="http://www.twitter.com/kpbsd"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-twitter.png" alt="twitter icon"></a>
-			<a href="https://www.youtube.com/KPBSDvideos"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-youtube.png" alt="youtube icon"></a>
-			<a href="https://kpbsd.zoom.us/"><img src="https://kpbsd.org/wp-content/uploads/quick-icons-zoom.png" alt="zoom icon"></a>
+			<a href="http://mail.g.kpbsd.org/"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-gmail.png" alt="gmail icon"></a>
+			<a href="http://drive.google.com/a/g.kpbsd.org/"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-google-drive.png" alt="google drive icon"></a>
+			<a href="https://kpbsd.zoom.us/"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-zoom.png" alt="zoom icon"></a>
 		</div>
 	</div>
 </div><!-- .site-branding -->
