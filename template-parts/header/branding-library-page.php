0c0bdb36ccee8467086956c8668e086be8486bcf
RoadyStudios <aroady@me.com>
2022-06-06 09:18:58 -0800

    Update branding-library-page.php

--- template-parts/header/branding-library-page.php
+++ template-parts/header/branding-library-page.php
@@ -14,3 +14,6 @@ namespace WP_Rig\WP_Rig;
 	<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> Library</a></h1>
 
 </div><!-- .site-branding -->
+<div class="breadcrumb-wrapper">
+	<?php echo do_shortcode( '[flexy_breadcrumb]' ); ?>
+</div>
