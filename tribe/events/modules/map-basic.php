07f2c20b3d97f1dd9e4198358b6e04de6801b3dc
RoadyStudios <aroady@me.com>
2022-07-19 10:58:51 -0800

    fixed title support for map iframe for accessibility

--- tribe/events/modules/map-basic.php
+++ tribe/events/modules/map-basic.php
@@ -28,6 +28,6 @@ if ( ! defined( 'ABSPATH' ) ) {
   width="<?php echo esc_attr( $width ); ?>"
   height="<?php echo esc_attr( $height ); ?>"
   frameborder="0" style="border:0"
-  src="<?php echo esc_url( $embed_url ); ?>" allowfullscreen>
   title="<?php the_title(); ?>"
+  src="<?php echo esc_url( $embed_url ); ?>" allowfullscreen>
 </iframe>
