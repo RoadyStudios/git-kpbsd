a4466a279c55e3dc7aa9c117dc8ee765b22acc62
RoadyStudios <aroady@me.com>
2023-08-11 11:30:53 -0800

    Update branding.php

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -11,21 +11,21 @@ namespace WP_Rig\WP_Rig;
 
 <div class="site-branding header-bkgd-wrapper">
 	<div class="box-logo-wrapper">
-		<?php the_custom_logo(); ?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" alt="Kenai Peninsula Borough School District Logo link" rel="home"><?php the_custom_logo(); ?></a>
 	</div>
 
 	<div class="box-title-wrapper">
-		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" alt="Kenai Peninsula Borough School District link" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 	</div>
 
 	<div class="box-icon-wrapper">
 		<div class="quick-icons">
-			<a href="https://kpbsd.instructure.com/" target="_blank"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-canvas.png" alt="canvas icon"></a>
-			<a href="http://mail.g.kpbsd.org/" target="_blank"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-gmail.png" alt="gmail icon"></a>
-			<a href="http://drive.google.com/a/g.kpbsd.org/" target="_blank"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-google-drive.png" alt="google drive icon"></a>
-			<a href="https://kpbsd.org/students-parents/powerschool/" target="_blank"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-powerschool.png" alt="powerschool icon"></a>
-			<a href="https://kpbsd.org/counselor-corner-and-onestop/" target="_blank"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-onestop.png" alt="onestop icon"></a>
-			<a href="https://kpbsd.zoom.us/" target="_blank"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-zoom.png" alt="zoom icon"></a>
+			<a href="https://kpbsd.instructure.com/" target="_blank" alt="canvas icon link"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-canvas.png" alt="canvas icon"></a>
+			<a href="http://mail.g.kpbsd.org/" target="_blank" alt="gmail icon link"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-gmail.png" alt="gmail icon"></a>
+			<a href="http://drive.google.com/a/g.kpbsd.org/" target="_blank" alt="google drive icon link"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-google-drive.png" alt="google drive icon"></a>
+			<a href="https://kpbsd.org/students-parents/powerschool/" target="_blank" alt="powerschool icon link"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-powerschool.png" alt="powerschool icon"></a>
+			<a href="https://kpbsd.org/counselor-corner-and-onestop/" target="_blank" alt="onestop icon link"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-onestop.png" alt="onestop icon"></a>
+			<a href="https://kpbsd.zoom.us/" target="_blank" alt="zoom icon link"><img src="https://kpbsd.org/wp-content/uploads/core-graphics/quick-icons-zoom.png" alt="zoom icon"></a>
 		</div>
 	</div>
 </div><!-- .site-branding -->
